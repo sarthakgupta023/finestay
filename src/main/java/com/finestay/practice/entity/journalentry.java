96deba81813166c21ac560409eae1db7946f2136
sarthak <sarthakgupta2303@gmail.com>
2026-02-17 02:31:19 +0530

    RestAPI

---
+++ src/main/java/com/finestay/practice/entity/journalentry.java
@@ -0,0 +1,35 @@
+package com.finestay.practice.entity;
+
+public class journalentry {
+
+    private Long id;
+    private String name;
+    private String working;
+
+    // Getter & Setter for id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter & Setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter & Setter for working
+    public String getWorking() {
+        return working;
+    }
+
+    public void setWorking(String working) {
+        this.working = working;
+    }
+}
